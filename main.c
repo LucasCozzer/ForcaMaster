3294674a67b5eb391f654e54e4b8b0a7b69a85ac
Lucas Cozzer <161613624+LucasCozzer@users.noreply.github.com>
2024-10-31 11:41:30 -0300

    inserindo lógica

--- main.c
+++ main.c
@@ -2,12 +2,16 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <ctype.h>
 // defines
 #define KICKS 10 // chutes
-#define MAXIMUM_ERRORS 6; // máximo de erros
+#define MAXIMUM_ERRORS 6 // máximo de erros
 #define SECRET_WORD_SIZE 5 + 1 // largura máxima de palavra secreta
 
 // procedimentos
+
+// limpar tela
+void clear_screen();
 
 
 void welcome_message (); // mensagem de boas vindas
@@ -20,9 +24,13 @@ void unmasking_word (int indice, char * mask, char * str); // substitui indice d
 
 void initializing_letter_mask (char * mask, char * wordSecret); // inicializando a mascara da string secreta
 
-void show_hud (); // mostrando hud do jogo para o usuário
+void show_hud (int errors, int attempts, char * maskString); // mostrando hud do jogo para o usuário
 
+void winner_msg (char * string_secret); // mensagem de ganhador
 
+void loser_msg (char * string_secret); // mensagem de perdedor
+
+void loser_msg_maxAttempts (char * string_secret); // mensagem de perdedor caso passe o limite de tentativas
 
 // funções
 
@@ -53,44 +61,89 @@ int main () {
 	word_initializer(stringSecret); // inicializando a palavra secreta do jogo
 	attempts = 0; // iniciando variavel que ira conter as tentativas
 	errors = 0; // iniciando variavel que ira conter os erros
+	hits = 0; // iniciando variavel que ira conter os erros
 	initializing_letter_mask(maskString, stringSecret); // inicializando a mascara da palavra
 
 	// iniciando lógica do jogo
-
 	do {
-		system("clear");
-		printf("\n\n %s \n\n", stringSecret);
-		show_hud(errors, attempts, maskString);
-		putchar('\n');
 
-		printf("Por favor, entre com uma letra: ");
+		// limpando a tela
+		clear_screen();
+		// mostrando o hud para o jogador
+		show_hud(errors, attempts, maskString);
+
+		// capturando entrada do usuario
+		putchar('\n');
+		printf("Entre com uma letra: => ");
 		inputLetter = getchar();
 
-		int i = existence_check(stringSecret, inputLetter);
-		if (i != 6) {
+		// verificação de letra
+		int i; // armazena o retorno (indice) da letra correta
+
+		// verificação de retorno da verificação
+		if ( (i = existence_check(stringSecret, inputLetter)) != 6) {
+
+			/*
+				caso retorno seja diferente do valor maximo + '\0' (6)
+			*/
+
+			hits++; // incrementando +1 acerto
+
+			// removendo a mascara da letra certa
+			unmasking_word(i, maskString, stringSecret);
+
+
+			// verificando se o número de acertos é igual ao tamanho da palavra secreta
 			if (hits == strlen(stringSecret)) {
-				printf("FIM! Venceu\n");
+
+				// limpando a tela
+				clear_screen();
+
+				// imprimindo mensagem de vitória
+				winner_msg(stringSecret);
+
 				return 0;
 			}
 
-			hits++;
-			unmasking_word(i, maskString, stringSecret);
 		} else {
 
-			if (errors == 6) {
-				printf("FIM! Perdeu\n");
-				break;
+			/*
+				caso retorno seja igual ao valor maximo + '\0' (6)
+			*/
+
+			errors++; // incrementa +1 erro
+
+			// verificação de quantidade de erros
+			if (errors == MAXIMUM_ERRORS) {
+
+				// limpando a tela
+				clear_screen();
+
+				// exibindo mensagem de perdedor
+				loser_msg(stringSecret);
+
+				return 1;
 			}
 
-			errors++;
 		}
 
-		
+		attempts++;
 
+
+		//limpando buffer
 		while(getchar() != '\n');
 
 	} while(attempts != KICKS);
 
+	// verificando se passou o máximo de tentativas
+	if (attempts >= KICKS) {
+
+		// limpando tela
+		clear_screen();
+
+		// exibindo mensagem de perdedor
+		loser_msg_maxAttempts(stringSecret);
+	}
 
 	return 0;
 }
@@ -105,8 +158,8 @@ int main () {
 int existence_check (char * str, char letter_input) {
 
 	for (int i = 0; *str; i++) {
-		if (*str == letter_input) return i; 
-		*str++;
+		if (tolower(*str) == tolower(letter_input)) return i; 
+		str++;
 
 	}
 
@@ -128,7 +181,7 @@ char * word_secret () {
 		"Casa",
 		"Paz",
 		"Flor",
-		"Céu",
+		"Ceu",
 		"Mar",
 		"Luz",
 		"Vida",
@@ -190,13 +243,12 @@ void initializing_letter_mask (char * mask, char * wordSecret) {
 	/*
 		Inicializando a mascara da plavara secreta
 	*/
+ 	int len = strlen(wordSecret); // Obtém o comprimento da palavra secreta
 
-
-	// percorrendo palavra secreta
-	for (int i = 0; *wordSecret; i++) {
-		*(mask + i) = '-'; // atribuindo o caratere '-' em cada indice do vetor mask
-		*(wordSecret++); // +1 byte
-	}
+    for (int i = 0; i < len; i++) {
+        mask[i] = '-'; // Preenche a máscara com '-'
+    }
+    mask[len] = '\0'; // Finaliza a string corretamente com o caractere nulo
 
 }
 
@@ -227,4 +279,53 @@ void unmasking_word (int indice, char * mask, char * str) {
 
 	*(mask + indice) = *(str + indice);
 
+}
+
+
+
+
+void clear_screen() {
+    #ifdef _WIN32
+        system("cls");  // Comando para Windows
+    #else
+        system("clear");  // Comando para Linux e MacOS
+    #endif
+}
+
+
+
+void winner_msg (char * string_secret) {
+	puts("\t ____________");
+	puts("\t |\t   |");
+	puts("\t |\t   |");
+	puts("\t |\t   O \t PARABÉNSSS! VOCÊ GANHOU O JOGO!");
+	printf("\t |\t  /|\\ \t Palavra secreta: %s\n", string_secret);
+	puts("\t |\t  / \\ ");
+	puts("\t |");
+	puts("\t |");
+	puts("\t |");
+}
+
+void loser_msg (char * string_secret) {
+	puts("\t ____________");
+	puts("\t |\t   |");
+	puts("\t |\t   |");
+	puts("\t |\t   O \t QUE PENA...VOCÊ PERDEU O JOGO!");
+	printf("\t |\t  /|\\ \t Palavra secreta: %s\n", string_secret);
+	puts("\t |\t  / \\ ");
+	puts("\t |");
+	puts("\t |");
+	puts("\t |");
+}
+
+void loser_msg_maxAttempts (char * string_secret) {
+	puts("\t ____________");
+	puts("\t |\t   |");
+	puts("\t |\t   |");
+	puts("\t |\t   O \t QUE PENA...VOCÊ PERDEU O JOGO!");
+	printf("\t |\t  /|\\ \t Palavra secreta: %s\n", string_secret);
+	puts("\t |\t  / \\ \t Motivo: Execedeu o limite de tentativas!!");
+	puts("\t |");
+	puts("\t |");
+	puts("\t |");
 }
